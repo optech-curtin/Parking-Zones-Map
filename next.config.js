e0cf224e471b30e4d4c58ff21c6e81824c94a005
Finn Morris <finn.morris@curtin.edu.au>
2025-04-16 11:18:57 +0800

    fixed login screen

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/ArcGIS_WebApp_Integration',
-  assetPrefix: '/ArcGIS_WebApp_Integration/',
+
 
   images: {
     unoptimized: true,
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import "@arcgis/core/assets/esri/themes/light/main.css";
 
 const inter = Inter({
   variable: "--font-inter",
