a13b0603ec588ca3ab94629ea8141b58205d3f22
Finn Morris <finn.morris@curtin.edu.au>
2025-06-06 07:39:26 +0800

    Added secret to different deploy context

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/Parking-Zones-Map',
-  assetPrefix: '/Parking-Zones-Map/',
+
   images: {
     unoptimized: true,
   },
