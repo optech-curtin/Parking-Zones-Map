6181f81a4f2d1bf46164e39caa2b73a2199b3129
Finn Morris <finn.morris@curtin.edu.au>
2025-06-06 08:42:22 +0800

    added next config (previous accident delete)

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-
+  basePath: '/Parking-Zones-Map',
+  assetPrefix: '/Parking-Zones-Map/',
   images: {
     unoptimized: true,
   },
